b1ae4a4535791544df453497cf02dc4824ce6cf3
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-04-12 23:50:49 +0330

    Update Globals.cs
    start_up_level to chose start up level :)
    new variables hover and assist
    adding Todo for saving process

--- MineSwepper/MineSwepperCodes/Globals.cs
+++ MineSwepper/MineSwepperCodes/Globals.cs
@@ -5,8 +5,20 @@ namespace MineSwepperGame.MineSwepperCodes
 {
     class Globals
     {
+        // Todo: Save
+        // Variables for save: hoverOn, assistOn, level, time | list of cell values(is_mine, is_clicked, is_flag).
+        // Each cell note: in process of creating a board we use this 3 values instead of probability.
+
         // Disable hover by set it to false
         public bool hoverOn = false;
+        public string hoverOn_text = "Turn off hover";
+        public string hoverOff_text = "Turn on hover";
+
+        // Assist
+        public bool assistOn = false;
+        public string assistOn_text = "Turn off assist";
+        public string assistOff_text = "Turn on assist";
+
 
         public string workingDirectory;
         public string Path;
@@ -31,6 +43,8 @@ namespace MineSwepperGame.MineSwepperCodes
 
         // mine counter font
         public Font font;
+
+        private int start_up_level = 0;
 
         public int curr_cube_size = 45;
         public int curr_board_width= 10;
@@ -57,11 +71,11 @@ namespace MineSwepperGame.MineSwepperCodes
 
             sizes = new int[][] { easy, medium, hard, ultra_hard };
 
-            curr_cube_size = sizes[0][0];
-            curr_board_width = sizes[0][1];
-            curr_board_height = sizes[0][2];
-            curr_mine_precent = sizes[0][3];
-            curr_mine_time = sizes[0][4];
+            curr_cube_size = sizes[start_up_level][0];
+            curr_board_width = sizes[start_up_level][1];
+            curr_board_height = sizes[start_up_level][2];
+            curr_mine_precent = sizes[start_up_level][3];
+            curr_mine_time = sizes[start_up_level][4];
             change_font_size();
         }
 
